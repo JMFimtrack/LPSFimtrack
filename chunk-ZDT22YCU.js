a6c3a068325814258f324e8de38c25f9d9cd6fd1
JMFimtrack <jesus.martinez@fimtrack.com.mx>
2024-10-11 16:08:07 -0600

    Auto-generated commit

---
+++ chunk-ZDT22YCU.js
@@ -0,0 +1,1 @@
+import{e as a}from"./chunk-2CEKSII7.js";import{U as t,W as o,mb as i}from"./chunk-QK4AK37H.js";var r=class e{router=t(a);ngOnInit(){this.router.navigate(["/buen/2024"])}static \u0275fac=function(n){return new(n||e)};static \u0275cmp=o({type:e,selectors:[["buenfin"]],standalone:!0,features:[i],decls:0,vars:0,template:function(n,u){}})};export{r as BuenfinComponent};
